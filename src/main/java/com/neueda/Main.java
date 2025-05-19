d8e8fa00a3d878a680b8105578d2cf17a44e5f51
Matt <matt.thornfield@neueda.com>
2025-05-19 14:21:40 +0000

    json

--- src/main/java/com/neueda/Main.java
+++ src/main/java/com/neueda/Main.java
@@ -1,10 +1,22 @@
 package com.neueda;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Main {
-    public static void main(String[] args) {
-        ObjectMapper om = new ObjectMapper();
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "Matt", 10000.0));
+        employees.add(new Employee(2, "Fergus", 20000.0));
+        employees.add(new Employee(3, "Teresa", 30000.0));
+
+        System.out.println(employees);
+
+        ObjectMapper om = new ObjectMapper();
+        String json = om.writerWithDefaultPrettyPrinter().writeValueAsString(employees);
+        System.out.println(json);
     }
 }
