664fad987672725a6ee3c876476c79a65bc45e1d
Matt <matt.thornfield@neueda.com>
2025-05-19 14:14:36 +0000

    first commit

---
+++ src/main/java/com/neueda/Employee.java
@@ -0,0 +1,4 @@
+package com.neueda;
+
+public class Employee {
+}
---
+++ src/main/java/com/neueda/Main.java
@@ -0,0 +1,10 @@
+package com.neueda;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjectMapper om = new ObjectMapper();
+
+    }
+}
